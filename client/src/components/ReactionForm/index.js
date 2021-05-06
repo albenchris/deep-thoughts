37914e41350202ab10e2aaff817bb1893301d5e7
albenchris <albenchris00@gmail.com>
2021-05-06 08:50:30 -0500

    added initial state and event handler functions to ReactionForm

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,15 +1,36 @@
 import React, { useState } from 'react';
 
 const ReactionForm = ({ thoughtId }) => {
+    const [reactionBody, setReactionBody] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
+
+    const handleChange = e => {
+        if (e.target.value.length <= 280) {
+            setReactionBody(e.target.value);
+            setCharacterCount(e.target.value.length);
+        }
+    };
+
+    const handleFormSubmit = async e => {
+        e.preventDefault();
+        setReactionBody('');
+        setCharacterCount(0);
+    }
+
     return (
         <div>
             <p className='m-0'>
                 Character Count: 0/280
             </p>
-            <form className='flex-row justify-center justify-space-between-md align-stretch'>
+            <form
+                className='flex-row justify-center justify-space-between-md align-stretch'
+                onSubmit={handleFormSubmit}
+            >
                 <textarea
                     placeholder='Leave a reaction to this thought...'
+                    value={reactionBody}
                     className='form-input col-12 col-md-9'
+                    onChange={handleChange}
                 ></textarea>
 
                 <button className='btn col-12 col-md-3' type='submit'>
